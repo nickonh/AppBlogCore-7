087d18ceb245f9f187d6dd95c610fd63f71ba6a2
NicolasKL <nickonh.arenas@gmail.com>
2023-05-25 22:35:48 -0400

    Se agrega el respositorio de Categoria y el Contenedor de trabajo para la implementacion del platron de diseño Repostitory Pattern.

---
+++ BlogCore.Data/Data/Repository/ContenedorTrabajo.cs
@@ -0,0 +1,33 @@
+﻿using AppBlogCore_7.Data;
+using BlogCore.Data.Data.Repository.IRepository;
+using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Data.Data.Repository
+{
+    public class ContenedorTrabajo : IContenedorTrabajo
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ContenedorTrabajo(ApplicationDbContext db)
+        {
+            _db = db;
+            RCategoria = new CategoriaRepository(_db);
+        }
+
+        public ICategoryRepository RCategoria { get; private set; }
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ BlogCore.Data/Data/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BlogCore.Data.Data.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Categoria>
+    {
+        IEnumerable<SelectListItem> GetListaCategoria();
+
+        void Update(Categoria categoria);
+    }
+}
